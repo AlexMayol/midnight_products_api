df1cd669d7bb3191ae6bc345b62176b6bc631eb7
unknown <alexmayolc@gmail.com>
2019-06-23 17:54:37 +0200

    swagger docs

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-  database: process.env.POSTGRES_URL
+  // database: process.env.POSTGRES_URL
+  database: 'postgres://mnunssew:ih_cn7KCpHx0At5z87m-sMPyU5sXaljo@manny.db.elephantsql.com:5432/mnunssew'
 };
 
